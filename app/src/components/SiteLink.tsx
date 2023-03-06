68cdf876ecf7683172e29ab58c9acb4565b96608
Claeb101 <calebarulandu@gmail.com>
2023-03-06 12:13:09 -0500

    footer

--- app/src/components/SiteLink.tsx
+++ app/src/components/SiteLink.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const SiteLink = ({txt, href, className=""}) => {
   return (
-    <Link href={href} passHref><a className={`${className}`}>{txt}</a></Link>
+    <Link href={href} passHref><a className={`underline text-white hover:text-blue transition-all ${className}`}>{txt}</a></Link>
   );
 }
 
--- app/src/components/footer.tsx
+++ app/src/components/footer.tsx
@@ -6,7 +6,7 @@ import SiteLink from './SiteLink';
 export const Footer = () => {
     return (
         <footer className="absolute bottom-0 text-white w-full pb-4 bg-navy-darkest bg-opacity-100">
-            <p className='mt-4 text-center text-base'>Made with 💖 by <SiteLink href={"https://arulandu.com"} txt="Alvan Caleb Arulandu" /> {' '} <SiteLink href={"https://crucialnet.org"} txt="Rushil Umaretiya" /> for HackTJ 10.0.</p>
+            <p className='mt-4 text-center text-base'>Made with 💖 by <SiteLink href={"https://arulandu.com"} txt="Alvan Caleb Arulandu" /> {' and '} <SiteLink href={"https://crucialnet.org"} txt="Rushil Umaretiya" /> for HackTJ 10.0.</p>
             <div className='flex justify-center'>
                 <p>Backend: <a className='inline-block align-middle' href="https://github.com/VitalityAI/therapist">
                     <img className='h-full' src="https://img.shields.io/github/last-commit/VitalityAI/therapist" />
